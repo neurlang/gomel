3a317bb7758aa964e211909c61842f1d8c51a3f9
neurlang <77860779+neurlang@users.noreply.github.com>
2025-04-26 14:28:07 +0200

    Phase package

---
+++ phase/impl.go
@@ -0,0 +1,400 @@
+package phase
+
+import "image"
+import "image/png"
+import "os"
+import "io"
+import "image/color"
+import "github.com/faiface/beep"
+import "github.com/faiface/beep/wav"
+import "github.com/mewkiz/flac"
+import "github.com/x448/float16"
+import "math"
+import "encoding/binary"
+
+func dumpbuffer(buf [][3]float64, mels int) (out []uint16) {
+	stride := len(buf) / mels
+
+	var mgc_max, mgc_min = [3]float64{(-99999999.), (-99999999.), (-99999999.)}, [3]float64{(9999999.), (9999999.), (9999999.)}
+
+	for l := 0; l < 2; l++ {
+		for x := 0; x < stride; x++ {
+			for y := 0; y < mels; y++ {
+				var w = buf[stride*y+x][l]
+				if w > mgc_max[l] {
+					mgc_max[l] = w
+				}
+				if w < mgc_min[l] {
+					mgc_min[l] = w
+				}
+			}
+		}
+	}
+	for x := 0; x < stride; x++ {
+		for y := 0; y < mels; y++ {
+			val0 := (buf[y+x*mels][0] - mgc_min[0]) / (mgc_max[0] - mgc_min[0])
+			val1 := (buf[y+x*mels][1] - mgc_min[1]) / (mgc_max[1] - mgc_min[1])
+			val := uint16(int(255*val0)) | uint16(int(255*val1))<<8
+
+			out = append(out, val)
+		}
+	}
+	return
+}
+
+func unpackBytesToFloat64(bytes []byte) float64 {
+	bits := binary.LittleEndian.Uint16(bytes)      // Read the bits from the byte slice
+	f := float64(float16.Frombits(bits).Float32()) // Convert uint64 bits to float64
+	return f
+}
+
+func loadpng(name string, reverse bool) (buf [][3]float64, samples, samplerate float64) {
+	// Open the PNG file
+	file, err := os.Open(name)
+	if err != nil {
+		println(err.Error())
+		return nil, 0, 0
+	}
+	defer file.Close()
+
+	// Decode the PNG file
+	img, err := png.Decode(file)
+	if err != nil {
+		println(err.Error())
+		return nil, 0, 0
+	}
+
+	// Get the bounds of the image
+	bounds := img.Bounds()
+	var floats []byte
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+
+			var color color.Color
+			if reverse {
+				// Get the color of the pixel at (x, y) with y-coordinate reversed
+				color = img.At(x, bounds.Max.Y-y-1)
+			} else {
+				// Get the color of the pixel at (x, y)
+				color = img.At(x, y)
+			}
+			r, g, b, _ := color.RGBA()
+
+			if x == 0 && y < 32 {
+				floats = append(floats, byte(b>>8))
+			}
+
+			val0 := float64(r>>8) / 255
+			val1 := float64(g>>8) / 255
+			val2 := float64(b>>8) / 255
+
+			val := [3]float64{val0, val1, val2}
+
+			buf = append(buf, val)
+		}
+	}
+	var mgc_max0, mgc_max1, mgc_max2, mgc_min0, mgc_min1, mgc_min2, samples_in_mel, sr =
+		unpackBytesToFloat64(floats[0:2]),
+		unpackBytesToFloat64(floats[2:4]),
+		unpackBytesToFloat64(floats[4:6]),
+		unpackBytesToFloat64(floats[6:8]),
+		unpackBytesToFloat64(floats[8:10]),
+		unpackBytesToFloat64(floats[10:12]),
+		unpackBytesToFloat64(floats[12:14]),
+		unpackBytesToFloat64(floats[14:16])
+
+	for i := range buf {
+		buf[i][0] = (buf[i][0]*(mgc_max0-mgc_min0) + mgc_min0)
+		buf[i][1] = (buf[i][1]*(mgc_max1-mgc_min1) + mgc_min1)
+		buf[i][2] = (buf[i][2]*(mgc_max2-mgc_min2) + mgc_min2)
+	}
+
+	samples = samples_in_mel * float64(bounds.Max.X-bounds.Min.X)
+	samplerate = sr
+	//dumpimage("test.png", buf, 160, reverse)
+	return
+}
+
+func packFloat16ToBytes(f float64) []byte {
+	var buf [2]byte
+	bits := float16.Fromfloat32(float32(f)).Bits()
+	binary.LittleEndian.PutUint16(buf[:], bits)
+	return buf[:]
+}
+func joinBytes(b ...[]byte) (ret []byte) {
+	for _, bytes := range b {
+		ret = append(ret, bytes...)
+	}
+	return ret
+}
+
+func dumpimage(name string, buf [][3]float64, mels int, reverse bool, samples_in_mel, sr float64) error {
+
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	stride := len(buf) / mels
+
+	img := image.NewNRGBA(image.Rect(0, 0, stride, mels))
+
+	var mgc_max, mgc_min = [3]float64{(-math.MaxFloat64),(-math.MaxFloat64),(-math.MaxFloat64)}, [3]float64{(math.MaxFloat64),(math.MaxFloat64),(math.MaxFloat64)}
+
+	for x := 0; x < stride; x++ {
+		for l := 0; l < 3; l++ {
+			for y := 0; y < mels; y++ {
+				var w = buf[stride*y+x][l]
+				if w > mgc_max[l] {
+					mgc_max[l] = w
+				}
+				if w < mgc_min[l] {
+					mgc_min[l] = w
+				}
+			}
+		}
+	}
+	var floats = joinBytes(
+		packFloat16ToBytes(mgc_max[0]),
+		packFloat16ToBytes(mgc_max[1]),
+		packFloat16ToBytes(mgc_max[2]),
+		packFloat16ToBytes(mgc_min[0]),
+		packFloat16ToBytes(mgc_min[1]),
+		packFloat16ToBytes(mgc_min[2]),
+		packFloat16ToBytes(samples_in_mel),
+		packFloat16ToBytes(sr),
+	)
+
+	//println(mgc_max[0], mgc_min[0])
+	for x := 0; x < stride; x++ {
+		for y := 0; y < mels; y++ {
+			var col color.NRGBA
+			val0 := (buf[y+x*mels][0] - mgc_min[0]) / (mgc_max[0] - mgc_min[0])
+			val1 := (buf[y+x*mels][1] - mgc_min[1]) / (mgc_max[1] - mgc_min[1])
+			val2 := (buf[y+x*mels][2] - mgc_min[2]) / (mgc_max[2] - mgc_min[2])
+
+			col.R = uint8(int(255 * val0))
+			col.G = uint8(int(255 * val1))
+			if x == 0 && y < len(floats) {
+				col.B = uint8(int(floats[y]))
+			} else {
+				col.B = uint8(int(255 * val2))
+			}
+			col.A = uint8(255)
+			if reverse {
+				img.SetNRGBA(x, mels-y-1, col)
+			} else {
+				img.SetNRGBA(x, y, col)
+			}
+		}
+	}
+
+	if err := png.Encode(f, img); err != nil {
+		f.Close()
+		return err
+	}
+
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func dumpwav(name string, data []float64, sr int) error {
+	noise := beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
+		if len(data) < len(samples) {
+			for i := range data {
+				samples[i][0] = data[i]
+				samples[i][1] = data[i]
+			}
+			l := len(data)
+			data = nil
+			return l, false
+		} else {
+			for i := range samples {
+				samples[i][0] = data[i]
+				samples[i][1] = data[i]
+			}
+			data = data[len(samples):]
+			return len(samples), true
+		}
+	})
+
+	// Create an output file
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Create a WAV encoder
+	err = wav.Encode(f, noise, beep.Format{
+		SampleRate:  beep.SampleRate(sr),
+		NumChannels: 1,
+		Precision:   2,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func loadwav(name string) (out []float64, sr float64) {
+	file, _ := os.Open(name)
+	defer file.Close()
+
+	// wavReader
+	stream, format, err := wav.Decode(file)
+	if err != nil {
+		println(err.Error())
+		return nil, 0
+	}
+
+	sr = float64(format.SampleRate)
+
+	// require wavReader
+	if stream == nil {
+		return nil, 0
+	}
+	var samples = make([][2]float64, 0, 1)
+	for {
+		samples = samples[0:1]
+		n, ok := stream.Stream(samples)
+		if !ok {
+			break
+		}
+		samples = samples[:n]
+		for i := 0; i < 1; i++ {
+			out = append(out, samples[i][0])
+		}
+	}
+	return
+}
+
+func loadflac(name string) (out []float64, sr float64) {
+	stream, err := flac.Open(name)
+	if err != nil {
+		println(err.Error())
+		return nil, 0
+	}
+	defer stream.Close()
+
+	// Iterate over the metadata blocks to find the StreamInfo block
+	var sampleRate = stream.Info.SampleRate
+	sr = float64(sampleRate)
+
+	for {
+		// Parse one frame of audio samples at the time, each frame containing one
+		// subframe per audio channel.
+		frame, err := stream.ParseNext()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
+		for _, subframe := range frame.Subframes {
+			for _, sample := range subframe.Samples {
+				out = append(out, float64(sample)/(256*256))
+			}
+			//break
+		}
+	}
+	return
+}
+
+func shrink(ospectrum [][3]float64, imels, omels int) (out [][3]float64) {
+	for i := range ospectrum {
+		j := i % imels
+		if j < omels {
+			out = append(out, ospectrum[i])
+		}
+	}
+	return
+}
+func grow(ospectrum [][3]float64, imels, omels int) (out [][3]float64) {
+	for i := range ospectrum {
+		j := i % imels
+		out = append(out, ospectrum[i])
+		if j + 1 == imels {
+			for k := imels ; k < omels; k++ {
+				out = append(out, ospectrum[i])
+			}
+		}
+	}
+	return
+}
+
+
+
+func spectral_normalize(buf [][3]float64) {
+	for l := 0; l < 3; l++ {
+		for i := range buf {
+			if buf[i][l] < 1e-10 {
+				buf[i][l] = 1e-10
+			}
+			buf[i][l] = float64(math.Log2(float64(buf[i][l])))
+		}
+	}
+}
+
+func spectral_denormalize(buf [][3]float64) {
+	for l := 0; l < 3; l++ {
+		for i := range buf {
+			buf[i][l] = float64(math.Exp2(float64(buf[i][l])))
+		}
+	}
+}
+
+func pad(buf []float64, filter int) []float64 {
+	// Calculate the current length of the buffer
+	currentLen := len(buf)
+
+	// Calculate the minimum target size (15 * filter)
+	minTargetSize := 15 * filter
+
+	// Calculate the required padding length
+	padLen := 0
+	if currentLen >= minTargetSize {
+		// Find the next target size which is a multiple of filter greater than currentLen
+		remainder := (currentLen - minTargetSize) % filter
+		if remainder != 0 {
+			padLen = filter - remainder - 1
+		}
+	} else {
+		// If currentLen is less than the minimum target size, pad to minTargetSize
+		padLen = minTargetSize - currentLen - 1
+	}
+
+	// Append the necessary padding
+	if padLen > 0 {
+		buf = append(buf, make([]float64, padLen)...)
+	}
+
+	return buf
+}
+
+func isPadded(originalLen, paddedLen, filter int) bool {
+	// Calculate the minimum target size
+	minTargetSize := 15 * filter
+
+	if originalLen >= minTargetSize {
+		// Calculate the remainder
+		remainder := (originalLen - minTargetSize) % filter
+		if remainder != 0 {
+			// Calculate the required padding length
+			padLen := filter - remainder - 1
+			// Check if the padded length matches the expected padded length
+			return paddedLen == originalLen+padLen
+		} else {
+			// No padding needed if remainder is zero
+			return paddedLen == originalLen
+		}
+	} else {
+		// Calculate the required padding length
+		padLen := minTargetSize - originalLen - 1
+		// Check if the padded length matches the expected padded length
+		return paddedLen == originalLen+padLen
+	}
+}
---
+++ phase/phase.go
@@ -0,0 +1,209 @@
+package phase
+
+import "github.com/r9y9/gossp/stft"
+import "github.com/mjibson/go-dsp/fft"
+import "errors"
+
+// Phase represents the configuration for generating phase-preserving spectrograms.
+type Phase struct {
+	NumFreqs  int
+	Window   int
+	Resolut  int
+	YReverse bool
+	// sample rate for output wav
+	SampleRate int
+}
+
+// NewPhase creates a new Phase instance with default values.
+func NewPhase() *Phase {
+	return &Phase{
+		NumFreqs:              768,
+		Window:               1280,
+		Resolut:              4096,
+	}
+}
+
+var ErrFileNotLoaded = errors.New("wavNotLoaded")
+
+// ToPhase generates a phase-preserving spectrogram from a wave buffer and returns the spectrogram buffer.
+func (m *Phase) ToPhase(buf []float64) ([][3]float64, error) {
+
+	buf = pad(buf, m.Window)
+
+	stft := stft.New(m.Window, m.Resolut)
+
+	spectrum := stft.STFT(buf)
+	var ospectrum [][3]float64
+
+	for i := range spectrum {
+		for j := 0; j < m.Resolut/2; j++ {
+
+			var v0 = spectrum[i][j+1]
+			var v1 = spectrum[i][m.Resolut-j-1]
+
+			var realn1 = imag(v0)
+			
+			var realm0 = real(v1)
+			var realm1 = imag(v1)
+
+			ospectrum = append(ospectrum, [3]float64{realn1, realm0, realm1})
+
+		}
+	}
+
+	ospectrum = shrink(ospectrum, m.Resolut/2, m.NumFreqs)
+	spectral_normalize(ospectrum)
+
+	return ospectrum, nil
+
+}
+
+
+
+func (m *Phase) undospectrum(ospectrum [][3]float64) (spectrum [][]complex128) {
+	spectrum = make([][]complex128, len(ospectrum)/(m.Resolut/2))
+
+	for i := range spectrum {
+		spectrum[i] = make([]complex128, m.Resolut)
+		for j := 0; j < m.Resolut/2; j++ {
+			index := i*(m.Resolut/2) + j
+			realn1 := ospectrum[index][0]
+			realm0 := ospectrum[index][1]
+			realm1 := ospectrum[index][2]
+
+			v0 := complex(realm0, realn1) // cos
+			v1 := complex(realm0, realm1) // sin
+
+			spectrum[i][j+1] = v0
+			spectrum[i][m.Resolut-j-1] = v1
+		}
+	}
+	return
+}
+
+func ISTFT(s *stft.STFT, spectrogram [][]complex128, numIterations int) []float64 {
+	frameShift := s.FrameShift
+	frameLen := len(spectrogram[0])
+	numFrames := len(spectrogram)
+	reconstructedSignal := make([]float64, frameLen+(numFrames-1)*frameShift)
+
+	for iter := 0; iter < numIterations; iter++ {
+
+		// ISTFT Step with new buffer
+		newReconstructed := make([]float64, frameLen+(numFrames-1)*frameShift)
+		//windowSum := make([]float64, len(newReconstructed))
+
+		for i := 0; i < numFrames; i++ {
+			buf := fft.IFFT(spectrogram[i])
+			for j := 0; j < frameLen; j++ {
+				pos := i*frameShift + j
+				if pos < len(newReconstructed) {
+					val := real(buf[j]) * s.Window[j]
+					newReconstructed[pos] += val
+				}
+			}
+		}
+
+		// Update reconstructedSignal for next iteration
+		reconstructedSignal = newReconstructed
+	}
+
+	return reconstructedSignal
+}
+
+// FromPhase generates a wave buffer from a Phase spectrogram and returns the wave buffer.
+func (m *Phase) FromPhase(ospectrum [][3]float64) ([]float64, error) {
+
+	stft1 := stft.New(m.Window, m.Resolut)
+
+
+	spectral_denormalize(ospectrum)
+	ospectrum = grow(ospectrum, m.NumFreqs, m.Resolut/2)
+
+	undo := m.undospectrum(ospectrum)
+
+	buf1 := ISTFT(stft1, undo, 1)
+
+	return buf1, nil
+}
+
+// LoadFlac loads mono flac file to sample vector
+func LoadFlac(inputFile string) []float64 {
+	mono, _ := loadflac(inputFile)
+	return mono
+}
+
+// LoadWav loads mono wav file to sample vector
+func LoadWav(inputFile string) []float64 {
+	mono, _ := loadwav(inputFile)
+	return mono
+}
+
+// SaveWav saves mono wav file from sample vector
+func SaveWav(outputFile string, vec []float64, sr int) error {
+	return dumpwav(outputFile, vec, sr)
+}
+
+func (m *Phase) Image(buf [][3]float64) []uint16 {
+	return dumpbuffer(buf, m.NumFreqs)
+}
+
+// ToPhaseFlac generates a Phase spectrogram from an input FLAC audio file and saves it as a PNG image.
+func (m *Phase) ToPhaseFlac(inputFile, outputFile string) error {
+
+	var buf, sr = loadflac(inputFile)
+	if len(buf) == 0 {
+		return ErrFileNotLoaded
+	}
+
+	ospectrum, err := m.ToPhase(buf)
+	if err != nil {
+		return err
+	}
+
+	dumpimage(outputFile, ospectrum, m.NumFreqs, m.YReverse, float64(len(buf)*m.NumFreqs)/float64(len(ospectrum)), float64(sr))
+
+	return nil
+}
+
+// ToPhaseWav generates a Phase spectrogram from an input WAV audio file and saves it as a PNG image.
+func (m *Phase) ToPhaseWav(inputFile, outputFile string) error {
+
+	var buf, sr = loadwav(inputFile)
+	if len(buf) == 0 {
+		return ErrFileNotLoaded
+	}
+
+	ospectrum, err := m.ToPhase(buf)
+	if err != nil {
+		return err
+	}
+
+	dumpimage(outputFile, ospectrum, m.NumFreqs, m.YReverse, float64(len(buf)*m.NumFreqs)/float64(len(ospectrum)), float64(sr))
+
+	return nil
+}
+
+func (m *Phase) ToWavPng(inputFile, outputFile string) error {
+
+	var buf, samples, samplerate = loadpng(inputFile, m.YReverse)
+	if len(buf) == 0 {
+		return ErrFileNotLoaded
+	}
+
+	owave, err := m.FromPhase(buf)
+	if err != nil {
+		return err
+	}
+
+	if int(samples) > 0 && isPadded(int(samples), len(owave), m.Window) && len(owave) > int(samples) {
+		owave = owave[0:int(samples)]
+	}
+	if samplerate != 0 && m.SampleRate == 0 {
+		m.SampleRate = int(samplerate)
+	}
+
+	dumpwav(outputFile, owave, m.SampleRate)
+
+	return nil
+}
